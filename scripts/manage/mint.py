e38e98af686322502c2e39d1134240c009dec839
Furion <_@furion.me>
2021-05-24 05:14:55 +0200

    Redeploy BSC

--- scripts/manage/mint.py
+++ scripts/manage/mint.py
@@ -8,7 +8,7 @@ c = Config.get()
 deployer_acc = c.get_deployer_account()
 
 
-def mint(mint_csv_path: str):
+def mint_multi(mint_csv_path: str):
     df = pd.read_csv(mint_csv_path)
     assert df.columns.tolist() == ["Address", "Amount"], "Invalid mint csv"
 
@@ -34,9 +34,26 @@ def mint(mint_csv_path: str):
 
     print(output)
 
+def mint_single(address: str, amount: int):
+    para = ParaToken.at(c.PARA_TOKEN)
+    assert deployer_acc.address == para.owner(), "Unauthorized account"
+
+    tx = para.mint(
+        address,
+        Wei(f'{amount} ether'),
+        {
+            "from": deployer_acc,
+            "gas_price": int(web3.eth.gasPrice * c.GAS_MULTIPLIER),
+        },
+    )
+    print(f'{address},{amount},{tx.txid}\n')
+
 
 def main():
-    mint("~/Downloads/minting_template.csv")
+    # mint_multi("~/Downloads/minting_template.csv")
+    # mint polkastarter
+    # mint_single(deployer_acc.address, 6_666_666)
+    pass
 
 
 if __name__ == "__main__":
